072f1ef9b5d750ec8828e59c9c676fd92bd4fae3
koshikawa <koshikawa@ppworks.jp>
2014-07-14 16:30:38 +0900

    Initial Commit

---
+++ HankakuTweet/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HankakuTweet
+//
+//  Created by koshikawa on 2014/07/14.
+//  Copyright (c) 2014年 ppworks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
